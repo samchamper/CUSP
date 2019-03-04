81ce7a374297a15e313677d08e1825c06ffa8cd8
Sam <samchamper@gmail.com>
2019-03-03 17:37:30 -0800

    Add gaussian interaction strength.

--- cpu_version.cxx
+++ cpu_version.cxx
@@ -1,3 +1,10 @@
+/* Author: Sam Chamoer 
+ * TODO: Switch to std::reduce
+ * TODO: switch to random generator
+ * TODO: prune dead.
+ * TODO: Offspring.
+ */
+
 #include <iostream>
 #include <math.h>
 #include <ctime>
@@ -6,19 +13,23 @@
 #include <vector>
 #include <numeric>
 #include <random>
+#include <execution>
+
 
 #include "funcs.h"
 #include "vtk_funcs.h"
 #include "Screen.h"
 
+#ifndef M_PI
+	#define M_PI					3.14159265358979323846  
+#endif
 
-#define START_POPULATION_SIZE 1000
-#define COMPETITION_THRESHOLD START_POPULATION_SIZE/50
-#define NUMBER_OF_GENERATION_STEPS 50
-#define INTERACTION_DISTANCE 0.05
-#define IND_SPEED 2   // Speed as a percentage of the world.
-//#define TORROIDAL_BOUNDARY
-#define GAUSSIAN_INTERACTION
+#define START_POPULATION_SIZE		1000
+#define INTERACTION_DISTANCE		0.05
+#define NUMBER_OF_GENERATION_STEPS	10
+#define IND_SPEED					2   // Speed as a percentage of the world.
+// #define TORROIDAL_BOUNDARY
+//#define GAUSSIAN_INTERACTION
 
 using std::cout;
 using std::cerr;
@@ -28,8 +39,16 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using std::chrono::microseconds;
 
-// Distribution for gaussian interaction.
-std::normal_distribution<double> normal(1.0, INTERACTION_DISTANCE);
+// For random generation.
+// TODO: Switch to this from rand.
+static std::random_device __randomDevice;
+static std::mt19937 __randomGen(__randomDevice());
+#ifdef GAUSSIAN_INTERACTION
+float COMPETITION_THRESHOLD = START_POPULATION_SIZE * M_PI *  INTERACTION_DISTANCE * INTERACTION_DISTANCE * 1.2;
+#else
+float COMPETITION_THRESHOLD = START_POPULATION_SIZE * M_PI *  INTERACTION_DISTANCE * INTERACTION_DISTANCE;
+#endif // GAUSSIAN_INTERACTION
+
 
 class World;
 
@@ -37,37 +56,36 @@ class Individual {
     public:
         int hp = 1000;
         int age;
-        double x, y;
+        float x, y;
         World *indworld;
-        vector<double> forceVector;
-        double force;
         inline void move();
-        inline void calcInteractionForce(int index);
 };
 
 class World {
     public:
-        World(int popsize);
+        World(int populationSize);
+		int popSize;
         vector<Individual> pop;
         void worldStep();
         void worldDraw(Screen screen);
+		vector<vector <float> > forceMatrix;
 };
 
-World::World(int popSize) {
+World::World(int populationSize) {
+	this->popSize = populationSize;
     pop.resize(popSize);
     for (int i = 0; i < popSize; ++i) {
-        pop[i].x = (double) rand() / static_cast<double>(RAND_MAX);
-        pop[i].y = (double) rand() / static_cast<double>(RAND_MAX);
+        pop[i].x = (float) rand() / static_cast<float>(RAND_MAX);
+        pop[i].y = (float) rand() / static_cast<float>(RAND_MAX);
         pop[i].age = 10;
         pop[i].indworld = this;
     }
 }
 
 void World::worldStep() {
-    int popSize = pop.size();
+	forceMatrix.resize(popSize, vector <float>(popSize));
     // Population moves, ages.
     for (int i = 0; i < popSize; ++i) {
-        pop[i].forceVector.resize(popSize);
         pop[i].move();
         // Reduce hp by a flat amount per time step.
         pop[i].hp -= 20;
@@ -75,12 +93,39 @@ void World::worldStep() {
         pop[i].age++;
     }
     // Calculate competion:
-    for (int i = 0; i < popSize; ++i)
-        pop[i].calcInteractionForce(i);
+    for (int i = 0; i < popSize; ++i) {
+		// Individual does not compete with itself.
+		forceMatrix[i][i] = 0;
+		// Calculate the interaction strength between this individual and all others.
+		// This is the time intensive part.
+		for (int j = i + 1; j < popSize; ++j) {
+			float dist = sqrt((pop[i].x - pop[j].x) * (pop[i].x - pop[j].x) + (pop[i].y - pop[j].y) * (pop[i].y - pop[j].y));
+#ifdef GAUSSIAN_INTERACTION
+			// Interaction strength as a function of distance on a normal curve.
+			if (dist < INTERACTION_DISTANCE * 4) {
+				forceMatrix[i][j] = exp(- (dist * dist) / (2 * INTERACTION_DISTANCE * INTERACTION_DISTANCE));
+				forceMatrix[j][i] = forceMatrix[i][j];
+			}
+			else {
+				forceMatrix[i][j] = 0;
+				forceMatrix[j][i] = 0;
+			}
+#else
+			if (dist < INTERACTION_DISTANCE) {
+				forceMatrix[i][j] = 1;
+				forceMatrix[j][i] = 1;
+			}
+			else {
+				forceMatrix[i][j] = 0;
+				forceMatrix[j][i] = 0;
+			}
+#endif
+		}
+	}
     // Apply competition:
     for (int i = 0; i < popSize; ++i) {
-        pop[i].force = std::accumulate(pop[i].forceVector.begin(), pop[i].forceVector.end(), 0);
-        if (pop[i].force > COMPETITION_THRESHOLD)
+		double force = std::reduce(std::execution::par, forceMatrix[i].begin(), forceMatrix[i].end());
+		if (force > COMPETITION_THRESHOLD)
             pop[i].hp -= 300;
     }
     // Prune the dead from the population:
@@ -102,8 +147,7 @@ void World::worldStep() {
 }
 
 void World::worldDraw(Screen screen) {
-    int popSize = pop.size();
-    for (int i = 0; i < popSize; ++i) {
+	for (int i = 0; i < popSize; ++i) {
         if (pop[i].hp > 0) {
             double r = clamp((1000 - (double) pop[i].hp) / 1000 * 1.1 - 0.1, 0, 1);
             double g = clamp((double) pop[i].hp / 1000 * 1.1 - 0.1, 0, 1);
@@ -127,53 +171,18 @@ void World::worldDraw(Screen screen) {
     }
 }
 
-inline void Individual::calcInteractionForce(int index){
-    /* Calculate the interaction strength between
-    * this individual and all others.
-    * This is the time intensive part. */
-    int popSize = indworld->pop.size();
-    forceVector.resize(popSize);
-    force = 0;
-    forceVector[index] = 0;
-    for (int i = index + 1; i < popSize; ++i) {
-        double dist = sqrt((x - indworld->pop[i].x) * (x - indworld->pop[i].x) +
-                           (y - indworld->pop[i].y) * (y - indworld->pop[i].y));
-        #ifdef GAUSSIAN_INTERACTION
-            // TODO: IMPLEMENT GAUSSIAN_INTERACTION INTERACTION.
-            // Interaction strength as a function of distance with a normal curve distribution.
-            if (dist < INTERACTION_DISTANCE * 4) {
-                forceVector[i] = normal(dist);
-                indworld->pop[i].forceVector[index] = normal(dist);
-            }
-            else {
-                forceVector[i] = 0;
-                indworld->pop[i].forceVector[index] = 0;
-            }
-        #else
-            if (dist < INTERACTION_DISTANCE) {
-                forceVector[i] = 1;
-                indworld->pop[i].forceVector[index] = 1;
-            }
-            else {
-                forceVector[i] = 0;
-                indworld->pop[i].forceVector[index] = 0;
-            }
-        #endif
-    }
-}
-
 inline void Individual::move() {
     // Toroidal boundary condition.
     #ifdef TORROIDAL_BOUNDARY
-        x = fmod(x, (((double) rand() / static_cast<double>(RAND_MAX)) - 0.5) / 50 * IND_SPEED);
-        y = fmod(y, (((double) rand() / static_cast<double>(RAND_MAX)) - 0.5) / 50 * IND_SPEED);
+        x = fmod(x, (((float) rand() / static_cast<float>(RAND_MAX)) - 0.5) / 50 * IND_SPEED);
+        y = fmod(y, (((float) rand() / static_cast<float>(RAND_MAX)) - 0.5) / 50 * IND_SPEED);
     #else
-        x += (((double) rand() / static_cast<double>(RAND_MAX)) - 0.5) / 50 * IND_SPEED;
+        x += (((float) rand() / static_cast<float>(RAND_MAX)) - 0.5) / 50 * IND_SPEED;
         if (x > 1)
             x = 2 - x;
         if (x < 0)
             x = 0 - x;
-        y += (((double) rand() / static_cast<double>(RAND_MAX)) - 0.5) / 50 * IND_SPEED;
+        y += (((float) rand() / static_cast<float>(RAND_MAX)) - 0.5) / 50 * IND_SPEED;
         if (y > 1)
             y = 2 - y;
         if (y < 0)
@@ -191,14 +200,12 @@ int main() {
 
     // Program specific:
     srand(time(NULL));
-    int starting_population = START_POPULATION_SIZE;
-    int num_steps = NUMBER_OF_GENERATION_STEPS;
     auto startZero = high_resolution_clock::now();
     auto start = high_resolution_clock::now();
     auto stop = high_resolution_clock::now();
     double dur;
-    World world(starting_population);
-    for (int i = 0; i < num_steps; ++i) {
+    World world(START_POPULATION_SIZE);
+    for (int i = 0; i < NUMBER_OF_GENERATION_STEPS; ++i) {
         start = high_resolution_clock::now();
         screen.clear();
         world.worldStep();
@@ -207,7 +214,6 @@ int main() {
         sprintf(str, "frame%03d" , i);
         WriteImage(image, str);
         cerr << "Done writing " << str << "." << endl;
-
         stop = high_resolution_clock::now();
         dur = duration_cast<microseconds>(stop - start).count();
         cout << "Step time: " << dur/1000000 << endl;
@@ -215,5 +221,6 @@ int main() {
     cerr << "Program finished!" << endl;
     dur = duration_cast<microseconds>(stop - startZero).count();
     cout << "Total run time: " << dur/1000000 << endl;
+	cin.get();
     return EXIT_SUCCESS;
 }
